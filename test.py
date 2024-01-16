877f22799b864a041269ce8d5789cd163dfe2fb8
pokoisthebest <kmatuszak15@gmail.com>
2024-01-16 05:21:37 +0100

    Added two functions getData and tabelizerData to test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import requests
 import pandas as pd
 import json
-API_KEY = 'FEYSO37WOLRNL4UZ'
+API_KEY = 'bca7fc15d195c77a01ddafa67513ab92'
 wig20 = {
     "ASSECOPOL" : "ACP",
     "ALLEGRO" : "ALE",
@@ -24,13 +24,21 @@ wig20 = {
     "PZU" : "PZU",
     "SANPL" : "SPL"
 }
+start_date = "2022-01-28"
+end_date = "2023-03-17"
+def getData(start_date: str, end_date: str, API_KEY, company: str):
+    url = f'https://financialmodelingprep.com/api/v3/historical-price-full/{company}?from={start_date}&to={end_date}&apikey={API_KEY}'
+    r = requests.get(url)
+    data = r.json()
+    return data
 
-# url = f'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol={wig20["ALIOR"]}&apikey={API_KEY}'
-# r = requests.get(url)
-# f = open('alr.json')
-with open('alr.json') as f:
-    data = json.load(f)
-# print(data['Monthly Time Series'])
-for key in data['Monthly Time Series']:
-    print(key,':' ,data['Monthly Time Series'][key]['4. close'])
-# f.close()
+def tabelarizeData(data):
+    data['symbol'] = {}
+    for day in data['historical']:
+        # print(f"{day['date']} : {day['close']} \n")
+        data['symbol'].update({day['date'] : day['close']})
+
+    return data['symbol']
+
+
+
