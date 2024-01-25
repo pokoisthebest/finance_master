de9b3cb02676191faa19955e6f55ad7304cce5f7
pokoisthebest <kmatuszak15@gmail.com>
2024-01-25 14:38:40 +0100

    added comments with func descriptions

--- historical_data.py
+++ historical_data.py
@@ -2,18 +2,39 @@ import requests
 import pandas as pd
 import json
 
-#TODO check how to declare default values to functions
+
 def getData(start_date: str, end_date: str, API_KEY, company: str):
+    """
+    Retrieves historical price data for a given company within a specified date range.
+
+    Parameters:
+    - start_date (str): The start date of the data range in the format 'YYYY-MM-DD'.
+    - end_date (str): The end date of the data range in the format 'YYYY-MM-DD'.
+    - API_KEY: The API key for accessing the financial data.
+    - company (str): The symbol or ticker of the company.
+
+    Returns:
+    - data (dict): A dictionary containing the historical price data for the company.
+    """
     url = f'https://financialmodelingprep.com/api/v3/historical-price-full/{company}?from={start_date}&to={end_date}&apikey={API_KEY}'
     r = requests.get(url)
     data = r.json()
     return data
 
 def tabelarizeData(data):
+    """
+    Converts historical data into a pandas DataFrame.
+
+    Args:
+        data (dict): A dictionary containing historical data.
+
+    Returns:
+        pandas.DataFrame: A DataFrame with columns 'Date' and 'Value', representing the date and closing value respectively.
+        None: If an exception occurs during the conversion process.
+    """
     data['symbol'] = {}
     try:
         for day in data["historical"]:
-            # print(f"{day['date']} : {day['close']} \n")
             data['symbol'].update({day['date'] : day['close']})
         df = pd.DataFrame(columns=['Date', 'Value'])
         df['Date'] =  data['symbol'].keys()
--- main.py
+++ main.py
@@ -3,34 +3,37 @@ import historical_data as hd
 import pandas as pd
 
 def main() -> int:
+    """
+    Main function that retrieves financial data for companies in the WIG20 index.
+
+    Returns:
+        int: The exit code of the function.
+    """
     API_KEY = 'bca7fc15d195c77a01ddafa67513ab92'
     wig20 = {
-    "ASSECOPOL" : "ACP",
-    "ALLEGRO" : "ALE",
-    "ALIOR" : "ALR",
-    "CDPROJEKT" : "CDR",
-    "CYFRPLSAT" : "CPS",
-    "DINOPL" : "DNP",
-    "JSW" : "JSW.WA",
-    "KGHM" : "KGH",
-    "KRUK" : "KRU",
-    "KETY" : "KTY",
-    "LPP" : "LPP",
-    "MBANK" : "MBK",
-    "ORANGEPL" : "OPL",
-    "PEPCO" : "PCO",
-    "PEKAO" : "PEO",
-    "PGE" : "PGE",
-    "PKNORLEN" : "PKN",
-    "PKOBP" : "PKO",
-    "PZU" : "PZU",
-    "SANPL" : "SPL"
-}
+        "ASSECOPOL" : "ACP",
+        "ALLEGRO" : "ALE",
+        "ALIOR" : "ALR",
+        "CDPROJEKT" : "CDR",
+        "CYFRPLSAT" : "CPS",
+        "DINOPL" : "DNP",
+        "JSW" : "JSW.WA",
+        "KGHM" : "KGH",
+        "KRUK" : "KRU",
+        "KETY" : "KTY",
+        "LPP" : "LPP",
+        "MBANK" : "MBK",
+        "ORANGEPL" : "OPL",
+        "PEPCO" : "PCO",
+        "PEKAO" : "PEO",
+        "PGE" : "PGE",
+        "PKNORLEN" : "PKN",
+        "PKOBP" : "PKO",
+        "PZU" : "PZU",
+        "SANPL" : "SPL"
+    }
     start_date = "2022-01-28"
     end_date = "2023-03-17"
-    # Assecopol = hd.getData(start_date, end_date, API_KEY, wig20["ASSECOPOL"])
-    # Assecopol = hd.tabelarizeData(Assecopol)
-    # print(Assecopol)
     wig20_tables = [key for key in wig20]
     wig20_tables = [hd.tabelarizeData(hd.getData(start_date, end_date, API_KEY, wig20[company])) for company in wig20_tables]
     [print(company) for company in wig20_tables]
