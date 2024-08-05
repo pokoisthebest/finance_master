5017dd0363e417e6db6342ce8a08db3109956bea
Matuszak <kacper.matuszak@wilhelmsen.com>
2024-08-05 16:39:27 +0200

    Added time lapse, todays price label and adjusted plots to show only data from given dates

--- gui.py
+++ gui.py
@@ -1,9 +1,11 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QPushButton, QWidget
+from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QPushButton, QWidget, QLabel
+from PyQt6.QtCore import QTimer, QDateTime
 from PyQt6 import uic
 from PyQt6.QtGui import QFont
 import sys
 import plots
 import historical_data as hd
+import datetime
 
 # start_date = "2022-01-28"
 # end_date = "2023-03-17"
@@ -18,8 +20,17 @@ class MainScreen(QMainWindow):
         self.wig20_button.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(1))
         self.return_button.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(0))
         self.company_windows = {}  # Store references to CompanyWindow instances
+        self.start_date = datetime.datetime(2017, 5, 5)  # Initial date
+        self.current_date = self.start_date
+        self.date_label.setText(self.current_date.strftime("%Y-%m-%d"))
+
+        # Setup a timer to update the date every 15 seconds
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.update_date)
+        self.timer.start(15000)  # 15 seconds
+
         if self.stackedWidget.currentIndex() == 1:
-            self.createButtons(wig20)  
+            self.createButtons(wig20)
 
     def createButtons(self, lista):
         buttons = [QPushButton(f"{item}") for item in lista]
@@ -39,8 +50,16 @@ class MainScreen(QMainWindow):
 
     def show_company(self, company_name):
         if company_name not in self.company_windows:
-            self.company_windows[company_name] = plots.PlotlyViewer(company_name)
+            self.company_windows[company_name] = plots.PlotlyViewer(company_name, self.start_date)
         self.company_windows[company_name].show()
+
+    def update_date(self):
+        self.current_date += datetime.timedelta(days=1)
+        while self.current_date.weekday() >= 5:  # Skip weekends (Saturday and Sunday)
+            self.current_date += datetime.timedelta(days=1)
+        self.date_label.setText(self.current_date.strftime("%Y-%m-%d"))
+        for company_window in self.company_windows.values():
+            company_window.update_date(self.current_date)
 
 app = QApplication(sys.argv)
 app.setFont(QFont("Modern", 20))
@@ -58,8 +77,4 @@ QPushButton{
 }
 """)
 window.show()
-
-try:
-    sys.exit(app.exec())
-except:
-    print('exiting')
+sys.exit(app.exec())
--- plots.py
+++ plots.py
@@ -1,22 +1,24 @@
-from PyQt6.QtWidgets import QVBoxLayout, QWidget, QPushButton, QHBoxLayout
-from PyQt6.QtWebEngineWidgets import QWebEngineView
-import plotly.graph_objects as go
 import pandas as pd
+import plotly.graph_objs as go
+from PyQt6.QtWidgets import QWidget, QVBoxLayout, QPushButton, QDoubleSpinBox, QHBoxLayout,QLabel
+from PyQt6.QtWebEngineWidgets import QWebEngineView
 
 class PlotlyViewer(QWidget):
-    def __init__(self, company_symbol):
+    def __init__(self, company_symbol, start_date):
         super().__init__()
         self.company_symbol = company_symbol
+        self.start_date = start_date
+        self.current_date = start_date  # Initialize current date
         self.setWindowTitle(f"{self.company_symbol}")
         self.setGeometry(100, 100, 1000, 600)
-
+        self.todays_price = QLabel("0")
         # Create layout
         layout = QVBoxLayout(self)
         
         # Create QWebEngineView widget
         self.web_view = QWebEngineView()
         layout.addWidget(self.web_view)
-
+        layout.addWidget(self.todays_price)
         # Generate and display the Plotly plot
         self.display_plot()
 
@@ -33,7 +35,6 @@ background-color: #15632a;
 }"""
         )
         buttons_layout.addWidget(button_buy)
-
         button_sell = QPushButton("SELL")
         button_sell.setStyleSheet(
             """
@@ -41,18 +42,22 @@ QPushButton{
 background-color: red;
 }
 QPushButton:hover{
-background-color: #4a1012;
+background-color: #db1d3f;
 }"""
         )
         buttons_layout.addWidget(button_sell)
-
+        buy_sell_value_field = QDoubleSpinBox()
+        buttons_layout.addWidget(buy_sell_value_field)
         layout.addLayout(buttons_layout)
         self.setLayout(layout)
 
     def display_plot(self):
         # Load data for the given company symbol
         df = pd.read_csv(f'data/{self.company_symbol}.csv')
-
+        df['Date'] = pd.to_datetime(df["Date"])
+        df = df.loc[(df['Date'] >= self.start_date) & (df['Date'] <= self.current_date)]
+        print(df.loc[df['Date'] == self.current_date])
+        
         fig = go.Figure(data=[go.Candlestick(x=df['Date'],
                                              open=df['Open'],
                                              high=df['High'],
@@ -60,7 +65,17 @@ background-color: #4a1012;
                                              close=df['Price'])])
 
         # Render the plot as HTML
+        fig.update_layout(
+            paper_bgcolor = '#0D1B2A',
+            font_color='white'
+        )
         html = fig.to_html(include_plotlyjs='cdn')
 
         # Load the HTML into the QWebEngineView
         self.web_view.setHtml(html)
+        self.todays_price.setText(f"Todays price: {str(df.loc[df['Date'] == self.current_date, 'Price'].values)}$")  
+        
+
+    def update_date(self, new_date):
+        self.current_date = new_date
+        self.display_plot()
